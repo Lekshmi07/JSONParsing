bf31b5abda244bab94eb4d439b8b030c79063f01
Lekshmi07 <rajalekshmi77277@gmail.com>
2018-05-30 20:33:45 +0530

    Initial commit

---
+++ app/src/main/java/com/example/lekshmi/gsheets1/util/Keys.java
@@ -0,0 +1,7 @@
+package com.example.lekshmi.gsheets1.util;
+
+public class Keys {
+    public static final String KEY_CONTACTS = "Sheet1";
+    public static final String KEY_NAME = "name";
+    public static final String KEY_COUNTRY = "country";
+}
